34e1c1b19aa82b9b5a8bf9dbdff5c3f5e05ecf15
Niahex <tqm6fa6b@duck.com>
2025-12-19 05:29:41 +0100

    Remove tests

--- src/cache.rs
+++ src/cache.rs
@@ -83,34 +83,3 @@ impl<T: Clone + Serialize + for<'de> Deserialize<'de>> Cache<T> {
         Ok(())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::path::PathBuf;
-
-    #[test]
-    fn test_cache_set_get() {
-        let cache: Cache<String> =
-            Cache::new(PathBuf::from("test_cache.json"), Duration::from_secs(3600));
-        cache.set("key1".to_string(), "value1".to_string());
-
-        assert_eq!(cache.get("key1"), Some("value1".to_string()));
-        assert_eq!(cache.get("nonexistent"), None);
-    }
-
-    #[test]
-    fn test_cache_clear() {
-        let cache: Cache<String> = Cache::new(
-            PathBuf::from("test_cache_clear.json"),
-            Duration::from_secs(3600),
-        );
-        cache.set("key1".to_string(), "value1".to_string());
-        cache.set("key2".to_string(), "value2".to_string());
-
-        cache.clear();
-
-        assert_eq!(cache.get("key1"), None);
-        assert_eq!(cache.get("key2"), None);
-    }
-}
